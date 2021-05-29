16f0319355c79961a10d050119ba77e344318fd9
ajaynagotha <ajaynagotha@gmail.com>
2021-05-29 08:28:39 +0530

    working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ export default function Footer() {
     return (
         <footer id="site-footer">
             <Nav className="footer-nav">
-                <Nav.Item><Nav.Link as={Link} to='tracking-options'>Tracking</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link>Options</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link>Tracking</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link as={Link} to='tracking-options'>Options</Nav.Link></Nav.Item>
                 <Nav.Item><Nav.Link>Home</Nav.Link></Nav.Item>
                 <Nav.Item><Nav.Link>Help</Nav.Link></Nav.Item>
             </Nav>
